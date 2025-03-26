7006fc50fb1bb7e4b8cd8e2de92172e499da6efe
Theo-D <theodessaigne@hotmail.fr>
2025-03-26 11:35:07 +0100

    removed condition for printing only printable characters

--- printChar.c
+++ printChar.c
@@ -9,11 +9,7 @@ int printChar(va_list args)
 {
 	char character = va_arg(args, int);
 
-	if (character >= 32 && character <= 126)
-	{
-		_putchar(character);
-		return (1);
-	}
-
-	return (-1);
+	_putchar(character);
+	
+	return (1);
 }
